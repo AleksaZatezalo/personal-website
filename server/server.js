dc4138791cb9c3a225c5c515877528afb12604f7
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-08-26 19:27:18 +0200

    More finished

--- server/server.js
+++ server/server.js
@@ -15,10 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS for development
+// CORS Configuration - Allow all origins
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
